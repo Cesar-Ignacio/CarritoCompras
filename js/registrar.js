2b29940fafec8ef3fbdf8e55d26f74a8a110ad11
cesar Tello <aquinocesarignacio@gmail.com>
2023-11-22 16:37:18 -0300

    StyleFinaPagRegis

--- js/registrar.js
+++ js/registrar.js
@@ -1,7 +1,7 @@
 /**Clases */
 class Usuario {
-    constructor(id,nombreUsuario, contrasenia, mail) {
-        this._id =id;
+    constructor(id, nombreUsuario, contrasenia, mail) {
+        this._id = id;
         this._nombreUsuario = nombreUsuario;
         this._contrasenia = contrasenia;
         this._mai = mail;
@@ -9,41 +9,85 @@ class Usuario {
 }
 
 /**Variables */
-let {_id:proximoID}=JSON.parse(localStorage.getItem("Usuarios")).pop();
-let btnGuardar=document.querySelector("#guardarUsuario");
-let listaUsuarios=JSON.parse(localStorage.getItem("Usuarios"));
-
-
+let btnGuardar = document.querySelector("#guardarUsuario");
+let listaUsuarios = JSON.parse(localStorage.getItem("Usuarios"));
+let inputID = document.querySelector("#idUsuario");
+let inputEmail = document.querySelector("#mailUsuario");
+let inputNombUsu = document.querySelector("#nombreUsuario");
+let inputPassUsu = document.querySelector("#passUsuario");
+let expreMail = /\S+@\S+\.\S+/;
+let expSolLet = /^[A-Za-z]+$/;
 /**Main */
-document.addEventListener("DOMContentLoaded",()=>{
-    let inputID=document.querySelector("#idUsuario");
-    inputID.setAttribute("value",proximoID+1);
-    
+document.addEventListener("DOMContentLoaded", () => {
+   
+    inputID.setAttribute("value",proximoId());    //cargarValores();
 })
 
 /**FUNCIONES */
- function validarMail(mail) {
-    let meErMa=document.querySelector(".mensajeErrorMail");
-    let expreMail=/\S+@\S+\.\S+/;
-    let estado=false;
-    (expreMail.test(mail))? (meErMa.innerText="",estado=true):meErMa.innerText="(formaro valido xxx@xxx.xx)";
+
+function validarDatos() {
+    let meErMa = document.querySelector(".mensajeErrorMail");
+    let meErNo = document.querySelector(".mensajeErrorNom");
+    let meErPa = document.querySelector(".mensajeErrorPass");
+    let estado = false;
+    let con = 0;
+    (expreMail.test(inputEmail.value)) ? (meErMa.innerText = "", con++) : meErMa.innerText = "(formaro valido xxx@xxx.xx)";
+    (inputNombUsu.value.length >= 4) ? (meErNo.innerText = "", con++) : meErNo.innerText = "(4 caractes como minimo)";
+    (inputPassUsu.value.length >= 4) ? (meErPa.innerText = "", con++) : meErPa.innerText = "(Por su seguridad el password debe contener mas de 4 caracteres)";
+    (con === 3) && (estado = true);
     return estado;
+
+}
+
+function limpiarCampos()
+{
+    inputID.setAttribute("value",proximoId());
+    inputEmail.value="";
+    inputNombUsu.value="";
+    inputPassUsu.value="";
+
+}
+
+function proximoId()
+{
+    let { _id: proximoID } = JSON.parse(localStorage.getItem("Usuarios")).pop();
+    return proximoID+1;
 }
 
 /**EVENTOS */
 
-btnGuardar.addEventListener("click",(e)=>{
+btnGuardar.addEventListener("click", (e) => {
     e.preventDefault();
-    let inputEmail=document.querySelector("#mailUsuario").value;
-    let inputNombUsu=document.querySelector("#nombreUsuario").value;
-    let inputPassUsu=document.querySelector("#passUsuario").value;
 
-    validarMail(inputEmail)&&(window.location.href="../index.html");
 
-  /*  listaUsuarios.push(new Usuario(proximoID+1,inputNombUsu,inputPassUsu,inputEmail))
-    localStorage.setItem("Usuarios",JSON.stringify(listaUsuarios));
-*/
-    // window.location.href="../index.html";
+    validarDatos() && (
+        listaUsuarios.push(new Usuario(proximoId(),inputNombUsu.value, inputPassUsu.value, inputEmail.value)),
+        localStorage.setItem("Usuarios", JSON.stringify(listaUsuarios)),
+        Swal.fire({
+            title: `Bienvenido ${inputNombUsu.value}`,
+            icon: "info",
+            html: `
+            Ya cuentas con un usuario y puedes iniciar sección ahora mismo,
+            <a href="/pages/login.html">login</a>
+             `,
+             showCloseButton: true,
+             showCancelButton: false,
+             focusConfirm: false,
+             showConfirmButton: false,    
+        }),
+        limpiarCampos()
+    );
+        
+   
 
 });
 
+inputNombUsu.addEventListener("keypress", (e) => {
+    // El nombre de usuario no puede tener espacios ni números
+    (expSolLet.test(e.key)) || e.preventDefault();
+});
+
+inputPassUsu.addEventListener("keypress", (e) => {
+    // La contaseña no puede tener espacios
+    (e.keyCode === 32) && (e.preventDefault())
+})
